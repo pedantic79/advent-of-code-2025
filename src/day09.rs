151ee1ae9bd822a63165d43ff8353d1a8e854a27
Dan Nguyen <pedantic@gmail.com>
2025-12-20 11:06:00 -0600

    Day09: Tuple 2 array. Remove circular_tuple_windows, remove simd

--- src/day09.rs
+++ src/day09.rs
@@ -1,22 +1,20 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
-use wide::CmpLt;
+use nom::{IResult, Parser, bytes::complete::tag, combinator, sequence::separated_pair};
+
+use crate::common::nom::{nom_i64, nom_lines, process_input};
+
+fn parse_line(s: &str) -> IResult<&str, [i64; 2]> {
+    combinator::map(separated_pair(nom_i64, tag(","), nom_i64), |(a, b)| [a, b]).parse(s)
+}
 
 #[aoc_generator(day9)]
-pub fn generator(input: &str) -> Vec<(i64, i64)> {
-    input
-        .lines()
-        .map(|line| {
-            let mut parts = line.split(',');
-            let x = parts.next().unwrap().parse().unwrap();
-            let y = parts.next().unwrap().parse().unwrap();
-            (x, y)
-        })
-        .collect()
+pub fn generator(input: &str) -> Vec<[i64; 2]> {
+    process_input(nom_lines(parse_line))(input)
 }
 
 #[aoc(day9, part1)]
-pub fn part1(inputs: &[(i64, i64)]) -> u64 {
+pub fn part1(inputs: &[[i64; 2]]) -> u64 {
     inputs
         .iter()
         .tuple_combinations()
@@ -25,45 +23,49 @@ pub fn part1(inputs: &[(i64, i64)]) -> u64 {
         .unwrap_or(0)
 }
 
-fn sort_tuples(x: (i64, i64), y: (i64, i64)) -> ((i64, i64), (i64, i64)) {
-    let a = (x.0.min(y.0), x.1.min(y.1));
-    let b = (x.0.max(y.0), x.1.max(y.1));
+fn sort_pairs(x: [i64; 2], y: [i64; 2]) -> [[i64; 2]; 2] {
+    let a = [x[0].min(y[0]), x[1].min(y[1])];
+    let b = [x[0].max(y[0]), x[1].max(y[1])];
 
-    (a, b)
+    [a, b]
 }
 
-fn tuples_to_array(a: (i64, i64), b: (i64, i64)) -> [i64; 4] {
-    [a.0, a.1, b.0, b.1]
+fn wide_less_than(a: [i64; 2], b: [i64; 2]) -> bool {
+    a[0] < b[0] && a[1] < b[1]
 }
 
-// This is a hotspot so we use SIMD to speed it up
-// 48.852 ms -> 37.214 ms on benchmark of part2
-pub fn wide_less_than(a: (i64, i64), d: (i64, i64), c: (i64, i64), b: (i64, i64)) -> bool {
-    let less = wide::i64x4::from(tuples_to_array(a, c));
-    let more = wide::i64x4::from(tuples_to_array(d, b));
-
-    less.simd_lt(more).all()
-    // a.0 < d.0 && a.1 < d.1 && c.0 < b.0 && c.1 < b.1
+fn distance(a: [i64; 2], b: [i64; 2]) -> u64 {
+    (a[0].abs_diff(b[0]) + 1) * (a[1].abs_diff(b[1]) + 1)
 }
 
-fn distance(a: (i64, i64), b: (i64, i64)) -> u64 {
-    (a.0.abs_diff(b.0) + 1) * (a.1.abs_diff(b.1) + 1)
+// This is simliar to the Itertools::circular_tuple_windows, but sort them because
+// it's faster.
+fn pair_list(inputs: &[[i64; 2]]) -> Vec<[[i64; 2]; 2]> {
+    let last = inputs.len() - 1;
+
+    let mut inputs_pairs = Vec::with_capacity(inputs.len());
+    for i in 0..last {
+        let [c, d] = sort_pairs(inputs[i], inputs[i + 1]);
+        inputs_pairs.push([c, d]);
+    }
+    let [c, d] = sort_pairs(inputs[last], inputs[0]);
+    inputs_pairs.push([c, d]);
+
+    inputs_pairs.sort_unstable();
+    inputs_pairs
 }
 
 #[aoc(day9, part2)]
-pub fn part2(inputs: &[(i64, i64)]) -> u64 {
+pub fn part2(inputs: &[[i64; 2]]) -> u64 {
+    let inputs_pairs = pair_list(inputs);
     inputs
         .iter()
         .tuple_combinations()
         .filter_map(|(x, y)| {
-            let (a, b) = sort_tuples(*x, *y);
+            let [a, b] = sort_pairs(*x, *y);
 
-            for (c, d) in inputs
-                .iter()
-                .circular_tuple_windows()
-                .map(|(x, y)| sort_tuples(*x, *y))
-            {
-                if wide_less_than(a, d, c, b) {
+            for &[c, d] in &inputs_pairs {
+                if wide_less_than(a, d) && wide_less_than(c, b) {
                     return None;
                 }
             }
